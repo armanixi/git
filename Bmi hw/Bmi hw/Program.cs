4b034e1f615a984f772c37e0be1ce1cbfe4b2fe5
DESKTOP-3P0SIHH\安 <armanixi@yahoo.com.tw>
2018-03-22 01:14:25 +0800

    no message

---
+++ Bmi hw/Bmi hw/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入你的身高");
+            float height = float.Parse(Console.ReadLine());
+            Console.Write("輸入你的體重");
+            float weight = float.Parse(Console.ReadLine());
+            float BMI = weight / height * height;
+            Console.WriteLine("你的BMI值為:" + BMI);
+            if (BMI > 31.5)
+            {
+                Console.WriteLine("過重,免兵役");
+            }
+            else if (BMI < 16.5)
+            {
+                Console.WriteLine("過輕,免兵役");
+            }
+            else
+            {
+                Console.WriteLine("BMI正常,必須當兵");
+            }
+
+            Console.Read();
+        }
+
+    }
+  }
+    
+
